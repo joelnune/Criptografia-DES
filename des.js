209de31021af7b50290e7142901ba60aed2fd625
joelnune <91698993+joelnune@users.noreply.github.com>
2023-10-23 16:56:47 -0300

    Cryptojs

--- des.js
+++ des.js
@@ -1,16 +1,11 @@
-// JOGA ISSO NO SHELL: $env:NODE_OPTIONS = "--openssl-legacy-provider"
-
-const crypto = require('crypto');
-const algorithm = 'des-ecb';
-
-const key = Buffer.from("d0e276d0144890d3", "hex");
-
-const cipher = crypto.createCipheriv(algorithm, key, null);
-let encrypted = cipher.update("Eu gosto da aula de segurança", 'utf8', 'hex');
-encrypted += cipher.final('hex');
-console.log("Encrypted: ", encrypted);
-
-const decipher = crypto.createDecipheriv(algorithm, key, null);
-let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-decrypted += decipher.final('utf8');
-console.log("Decrypted: ", decrypted);
+const CryptoJS = require('crypto-js');
+const key = '12345678'; 
+const data = 'Hello, World!';
+//criptografar
+const encrypted = CryptoJS.DES.encrypt(data, key);
+const encryptedBase64 = encrypted.toString();
+console.log('Encrypted Data:', encryptedBase64);
+//descriptografar
+const decrypted = CryptoJS.DES.decrypt(encrypted, key);
+const decryptedData = decrypted.toString(CryptoJS.enc.Utf8);
+console.log('Decrypted Data:', decryptedData);
